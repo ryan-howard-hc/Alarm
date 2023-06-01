d08b09acd8d092391240854dd6b19be829a7129f
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-01 13:01:37 -0400

    trying to add actual clock, css wont work for now

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function currentTime(){
+    var date = new Date();
+    var year = date.getFullYear(); 
+    var month = date.getMonth(); //0-12
+    var day = date.getDay(); //0-31
+    var hour = date.getHours(); //0-23
+    var minute = date.getMinutes(); //0-59
+    var second = date.getSeconds(); //0-59
+    var session = "AM"; 
+    var military = ""; //no am or pm, just a number/date
+
+};
--- pseudocode.md
+++ pseudocode.md
@@ -7,6 +7,7 @@ Create an alarm clock from scratch that runs all the time without refreshing
 - Display current time in digital format in Hours:Minutes:Seconds along with MDY if possible
 - Pick a time for alarm clock to function
 - Make sure alarm refreshes without actually refreshing the page
+
 ### POSSIBLE INIT Extras:
 - MDY
 - Toggle between Normal time and Military time
